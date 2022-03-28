d916f9a94bd4904434e6555662e6ac020bed3548
Sounghun Um <korea970112@naver.com>
2022-03-29 03:10:35 +0900

    예제파일에 회원가입 & 타임리프적용까지 (정규식 x)

---
+++ src/main/java/com/eungsoo/blog/service/ContentsService.java
@@ -0,0 +1,27 @@
+package com.eungsoo.blog.service;
+
+import com.eungsoo.blog.models.Contents;
+import com.eungsoo.blog.repository.ContentsRepository;
+import com.eungsoo.blog.models.ContentsRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ContentsService {
+
+    private final ContentsRepository ContentsRepository;
+
+    @Transactional
+    public Long update(@PathVariable Long id, @RequestBody ContentsRequestDto requestDto) {
+        Contents Contents = ContentsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+        );
+        Contents.update(requestDto);
+        return Contents.getId();
+    }
+}
---
+++ src/test/java/com/myblog/sounghun/SounghunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myblog.sounghun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SounghunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
